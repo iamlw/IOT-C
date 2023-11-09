d6d4ab6f11b9807082d00efab917bd055a973fbf
liuwei <liuwei@example.com>
2023-11-09 15:21:31 +0800

    02

---
+++ test-demo/test02.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("hello worl66");
+    printf("hello worl66");
+    printf("hello worl66");
+    printf("hello worl66");
+    return 0;
+}
