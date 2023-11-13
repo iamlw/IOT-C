fdc4279e92facfe62d96d9fa0c6ebadd3de1de8f
liuwei <liuwei@example.com>
2023-11-13 14:26:14 +0800

    delete

--- allbase/main.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-#include"myutils.h"
-int main(int argc, char const *argv[])
-{
-    test();
-    return 0;
-}
--- allbase/myutils.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-void test()
-{
-    printf("test函数被调用了\n");
-}
